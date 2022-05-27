1d4ed5fbd10554fd87488ad88a3b004e9ce5fea9
neonilakunyk <n.kunyk-2025@kpi.ua>
2022-05-28 01:05:28 +0300

    Fixed getPermutation

--- src/modules/shared/helpers/array/get-permitation.helper.ts
+++ src/modules/shared/helpers/array/get-permitation.helper.ts
@@ -5,11 +5,11 @@ const getPermutation = <T extends IJob>(array: T[]): T[][] => {
     const firstElement = array[0];
     const returnedArray = getPermutation(array.slice(1));
     const permutedArray = [] as T[][];
-    const temporaryArray = [] as T[];
+    let temporaryArray = [] as T[];
     const elementLength = returnedArray[0].length;
     for (let i = 0; i < returnedArray.length; i++)
       for (let j = 0; j <= elementLength; j++) {
-        temporaryArray.splice(0, temporaryArray.length, ...returnedArray[i].slice(0));
+        temporaryArray = returnedArray[i].slice(0);
         temporaryArray.splice(j, 0, firstElement);
         permutedArray.push(temporaryArray);
       }
